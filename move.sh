4585f66ea0f80e2f9d98d156498673b454767f38
hassan shahid <overlordofdoom12345@gmail.com>
2019-10-27 18:39:29 +0000

    sperate source from device tree with a move script

---
+++ move.sh
@@ -0,0 +1,6 @@
+lineage_dir=../lineage17
+
+cp -r ./device/samsung $lineage_dir/device
+cp -r ./hardware/samsung $lineage_dir/hardware/
+cp -r ./vendor/samsung $lineage_dir/vendor/
+cp -r ./vendor/addons $lineage_dir/vendor/
